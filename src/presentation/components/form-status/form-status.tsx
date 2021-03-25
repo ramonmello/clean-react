bdba14562d75044869c97a8971c316b1b09b30a9
Ramon Mello <ramonomello@gmail.com>
2021-03-24 23:03:40 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import S from './form-status-styles.scss'
 import { Spinner } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={S.errorWrap}>
-      <Spinner className={S.spinner} />
-      <span className={S.error}>Erro</span>
+    <div data-testid="error-wrap" className={S.errorWrap}>
+      {isLoading && <Spinner className={S.spinner} />}
+      {errorMessage && <span className={S.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import S from './login-styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
   return (
     <div className={S.login}>
       <LoginHeader />
-      <form className={S.form}>
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={S.submit} type="submit">Entrar</button>
-        <span className={S.link}>Criar conta</span>
-        <FormStatus />
-      </form>
+      <Context.Provider value={state}>
+        <form className={S.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <button className={S.submit} type="submit">Entrar</button>
+          <span className={S.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+
       <Footer />
     </div>
   )
